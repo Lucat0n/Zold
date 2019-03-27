b7c4fa0a465ba54746323f74744e0ea47d48eeeb
Adam Tomczak <43048737+Adven97@users.noreply.github.com>
2019-03-27 22:42:00 +0100

    Praca Domowa 2

---
+++ Praca Domowa 2/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bt = new Bitmap(@"..\\..\\shrek.jpg");
+            int wdt = 160;
+
+            for (int y = 0; y < bt.Height; y++)
+            {
+                for (int x = 0; x < bt.Width; x++)
+                {
+                    int tem = x%wdt;
+                    if (tem >0 && tem <wdt/2)
+                    {
+                        
+                        Color c = bt.GetPixel(x, y);
+
+                        int r = c.R;
+                        int g = c.G;
+                        int b = c.B;
+                        int avg = (r + g + b) / 3;
+                        bt.SetPixel(x, y, Color.FromArgb(avg, avg, avg));
+                    }
+
+                }
+            }
+            bt.Save(@"..\\..\\shrek-out.bmp");
+
+        }
+
+        
+
+
+    }
+}
