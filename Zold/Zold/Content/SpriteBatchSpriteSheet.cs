cafecbe0a1614797047ff3f75c34c9d0dd16513c
209450 <209450@edu.p.lodz.pl>
2019-04-16 02:59:48 +0200

    Dodanie klasy do obsługi animacji ze spritesheetu

--- Zold/Zold/Content/SpriteBatchSpriteSheet.cs
+++ Zold/Zold/Content/SpriteBatchSpriteSheet.cs
@@ -3,45 +3,66 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 
-public class SpriteBatchSpriteSheet : SpriteBatch
+namespace Content
 {
-    public Texture2D SpriteSheet { get; private set; }
-
-    public int Rows { get;  set; }
-
-    public int Cols { get; set; }
-
-    public int TextureWidthPixels { get; set; }
-
-    public int TextureHeightPixels { get; set; }
-
-    public int SpriteSheetWidth { get { return TextureWidthPixels*Cols; }}
-
-    public int SpriteSheetHeight { get { return TextureHeightPixels*Rows; }}
-
-    private Rectangle FrameRectangle;
-
-    private Rectangle TextureRectagle;
-
-    public SpriteBatchSpriteSheet(GraphicsDevice graphicsDevice ,Texture2D spriteSheet, int rows, int cols, int textureWidthPixels, int textureHeightPixels) : base(graphicsDevice)
+    public class SpriteBatchSpriteSheet : SpriteBatch
     {
-        SpriteSheet = spriteSheet;
-        Rows = rows;
-        Cols = cols;
-        TextureWidthPixels = textureWidthPixels;
-        TextureHeightPixels = textureHeightPixels;
+        public Texture2D SpriteSheet { get; private set; }
 
-        FrameRectangle = new Rectangle(0, 0, SpriteSheetWidth, SpriteSheetHeight);
-        TextureRectagle = new Rectangle(0, 0, TextureWidthPixels, TextureHeightPixels);
+        public int Rows { get; set; }
+
+        public int Cols { get; set; }
+
+        public int TextureWidthPixels { get; set; }
+
+        public int TextureHeightPixels { get; set; }
+
+        public int SpriteSheetWidth { get { return TextureWidthPixels * Cols; } }
+
+        public int SpriteSheetHeight { get { return TextureHeightPixels * Rows; } }
+
+        private Rectangle[,] Frames;
+
+        private Dictionary<string, Animation> Animations = new Dictionary<string, Animation>();
+
+        public SpriteBatchSpriteSheet(GraphicsDevice graphicsDevice, Texture2D spriteSheet, int rows, int cols, int textureWidthPixels, int textureHeightPixels) : base(graphicsDevice)
+        {
+            SpriteSheet = spriteSheet;
+            Rows = rows;
+            Cols = cols;
+            TextureWidthPixels = textureWidthPixels;
+            TextureHeightPixels = textureHeightPixels;
+
+            Frames = new Rectangle[rows, cols];
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    Frames[i,j] = new Rectangle(j*textureWidthPixels, i*TextureHeightPixels, textureWidthPixels, textureHeightPixels);
+        }
+
+        public void Draw(Vector2 place, int x, int y)
+        {
+            base.Draw(SpriteSheet, place, Frames[x,y], Color.White);
+        }
+
+        public void MakeAnimation(int rowNumber, String name, int time)
+        {
+            List<Rectangle> tmp = new List<Rectangle>();
+            for (int i = 0; i < Cols; i++) tmp.Add(Frames[rowNumber, i]);
+            Animations.Add(name, new Animation(tmp, time, Cols));
+        }
+
+        public void PlayFullAniamtion(Vector2 place, String name, GameTime gameTime)
+        {
+            try
+            {
+                base.Draw(SpriteSheet, place, Animations[name].Update(gameTime), Color.White);
+            }
+            catch (KeyNotFoundException e)
+            {
+                base.Draw(SpriteSheet, place, Frames[0, 0], Color.Pink);
+            }
+        }
     }
 
-    public void Draw(Vector2 place, int x, int y)
-    {
-        x *= TextureWidthPixels;
-        y *= TextureHeightPixels;
-        TextureRectagle.Offset(x,y);
-        base.Draw(SpriteSheet, place, TextureRectagle, Color.White);
-        TextureRectagle.Offset(-x, -y);
-    }
 
 }
