8d1677910b10e026b918bcb1cf107152bf82178d
Wiktor <wiktor756@gmail.com>
2019-04-17 17:10:33 +0200

    Oddzielenie pierwszego rodzaju przeciwnika

---
+++ Zold/Zold/Combat/Mob.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Combat
+{
+    class Mob : Enemy
+    {
+        private Timer attackTimer;
+
+        public Mob(Player player, Vector2 position) : base(player, position)
+        {
+            attackTimer = new Timer();
+            attackTimer.Interval = 1000;
+            attackTimer.Elapsed += new ElapsedEventHandler(Attack);
+        }
+
+        public override void AI(GameTime gameTime)
+        {
+            Speed = 60f * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            CalcDirection();
+
+            if (attackTimer.Enabled == true)
+                Action = "Attacking";
+            else
+                Action = "Idle";
+
+            if (Distance <= 50 && attackTimer.Enabled == false)
+            {
+                attackPosition = player.GetCenterPosition();
+                attackTimer.Enabled = true;
+            }
+            else if (Distance <= 200 && attackTimer.Enabled == false)
+            {
+                Action = "Moving";
+                Move();
+            }
+        }
+
+        public override void Move()
+        {
+            position.X += direction.X * Speed;
+            position.Y += direction.Y * Speed;
+        }
+
+        private void Attack(object source, ElapsedEventArgs e)
+        {
+            if (player.CheckPointCollision(attackPosition))
+                player.Hp -= Damage;
+            attackTimer.Enabled = false;
+        }
+
+
+    }
+}
