19224a51710c59098c9334b7f06468aef962cf99
Wiktor <wiktor756@gmail.com>
2019-05-16 11:39:37 +0200

    Dodanie szablonu dystansowego przeciwnika

---
+++ Zold/Zold/Screens/Implemented/Combat/CombatObjects/Characters/Enemies/Ranged.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Zold.Utilities;
+
+namespace Zold.Screens.Implemented.Combat.CombatObjects.Characters.Enemies
+{
+    class Ranged : Enemy
+    {
+        public Ranged(Player player, Vector2 position, SpriteBatchSpriteSheet SpriteBatchSpriteSheet, int width, int height) : base(player, position, SpriteBatchSpriteSheet, width, height)
+        {
+        }
+
+        public override void AI(GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Animation(GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Move()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
