165616ecf5915cad46121cde62eeaf56135988e3
209450 <209450@edu.p.lodz.pl>
2019-05-25 02:05:25 +0200

    placeholder dla grafiki combatu

---
+++ Zold/Zold/Content/graphic/combat/combat_city.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="combat_city" tilewidth="32" tileheight="32" tilecount="135" columns="15">
+ <image source="combat_city.png" width="480" height="288"/>
+</tileset>
