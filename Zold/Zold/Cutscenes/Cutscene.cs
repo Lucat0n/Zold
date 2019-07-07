635920e738700870f1e50b35bd10b8f9f41115a1
209450 <209450@edu.p.lodz.pl>
2019-07-07 20:52:51 +0200

    Podstawowa struktura obsługi cutscenek

---
+++ Zold/Zold/Cutscenes/Cutscene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zold.Cutscenes
+{
+    class Cutscene
+    {
+        private List<CutsceneCommand> listOfCommands = new List<CutsceneCommand>();
+
+        public Cutscene()
+        {
+        }
+
+        public void AddCommand(CutsceneCommand cutsceneCommand)
+        {
+            listOfCommands.Add(cutsceneCommand);
+        }
+
+        public void PlayCutscene()
+        {
+            foreach(CutsceneCommand cutsceneCommand in listOfCommands)
+            {
+                cutsceneCommand.execute();
+            }
+        }
+    }
+}
